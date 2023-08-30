120cfb8030bdade230bc80406f7809c82867aefd
Tristan Barnes <tristan.barnes@colorado.edu>
2023-08-30 14:38:56 +0000

    first and final commit

---
+++ hw0.py
@@ -0,0 +1,15 @@
+#DO NOT CHANGE THE NAMES OF THE FILE, CLASS or METHOD
+class Exam:
+    def PassFail(self, exam1, exam2, exam3, exam4):
+        total = exam1 + exam2 + exam3 + exam4
+        i = 0
+        while total/6 < 55:
+            total += 1
+            i += 1
+        return i
+# For testing your code uncomment below lines.
+
+e = Exam()
+print(e.PassFail(15, 65, 55, 94))
+
+# Comment or delete the above test code before submitting.
